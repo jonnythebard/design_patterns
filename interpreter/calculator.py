cec2d37f78453ec32d924d3e8acc92a9967d53b4
Jonny Park <jonnythebard9@gmail.com>
2020-09-26 17:56:53 +0900

    add interpreter

---
+++ interpreter/calculator.py
@@ -0,0 +1,18 @@
+from lexer import lex
+from parser import parse
+
+
+def eval(input):
+    tokens = lex(input)
+    print(' '.join(map(str, tokens)))
+
+    parsed = parse(tokens)
+    print(f'{input} = {parsed.value}')
+
+
+if __name__ == '__main__':
+    eval('(13+4)-(12+1)')
+    eval('1+(3-4)')
+
+    # this won't work
+    eval('1+2+(3-4)')
---
+++ interpreter/parser.py
@@ -0,0 +1,62 @@
+from enum import Enum
+from lexer import Token
+
+
+class Integer:
+    def __init__(self, value):
+        self.value = value
+
+
+class BinaryOperation:
+    class Type(Enum):
+        ADDITION = 0
+        SUBTRACTION = 1
+
+    def __init__(self):
+        self.type = None
+        self.left = None
+        self.right = None
+
+    @property
+    def value(self):
+        if self.type == self.Type.ADDITION:
+            return self.left.value + self.right.value
+        elif self.type == self.Type.SUBTRACTION:
+            return self.left.value - self.right.value
+
+
+def parse(tokens):
+    result = BinaryOperation()
+    have_lhs = False
+    i = 0
+    while i < len(tokens):
+        token = tokens[i]
+
+        if token.type == Token.Type.INTEGER:
+            integer = Integer(int(token.text))
+            if not have_lhs:
+                result.left = integer
+                have_lhs = True
+            else:
+                result.right = integer
+        elif token.type == Token.Type.PLUS:
+            result.type = BinaryOperation.Type.ADDITION
+        elif token.type == Token.Type.MINUS:
+            result.type = BinaryOperation.Type.SUBTRACTION
+        elif token.type == Token.Type.LPAREN:  # note: no if for RPAREN
+            j = i
+            while j < len(tokens):
+                if tokens[j].type == Token.Type.RPAREN:
+                    break
+                j += 1
+            # preprocess subexpression
+            subexpression = tokens[i + 1:j]
+            element = parse(subexpression)
+            if not have_lhs:
+                result.left = element
+                have_lhs = True
+            else:
+                result.right = element
+            i = j  # advance
+        i += 1
+    return result
